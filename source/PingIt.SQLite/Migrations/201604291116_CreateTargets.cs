7d16044a3ef5a8dd58f650120781bfc82dee280a
Mads Nyborg <nyborg.mads@gmail.com>
2016-04-29 11:33:16 +0200

    draft on migration and sqlite

---
+++ source/PingIt.SQLite/Migrations/201604291116_CreateTargets.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace PingIt.Store.SQLite.Migrations
+{
+    [Migration(201604291116)]
+    public class _201604291116_CreateTargets : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Targets")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Url").AsString(1000).NotNullable();
+        }
+    }
+}

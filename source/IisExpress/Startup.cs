40a09d8c03b60e8f7702b3ad7d0ea22b2c81af37
John10 Korsnes <johnkors@gmail.com>
2016-04-30 13:23:37 +0200

    removed ref to c:\tmp in connectionstr

--- source/IisExpress/Startup.cs
+++ source/IisExpress/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-
+using System.Configuration;
 using System.Data.SQLite;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Nancy;
 using Nancy.TinyIoc;
 using Owin;
+using PingIt.Store.SQLite;
+using PingOwin;
 using PingOwin.Web;
 
 [assembly: OwinStartup(typeof(IisExpress.Startup))]
@@ -16,9 +18,24 @@ namespace IisExpress
     {
         public void Configuration(IAppBuilder app)
         {
-            var cmd = new SQLiteCommand();
-            cmd.CommandText = "This is adummy";
+            var settings = new ConfigFileSettings();
+            var migrator = new Migrator(settings);
+            migrator.Migrate();
             app.UsePingOwinFrontend();
         }
     }
+
+    internal class ConfigFileSettings : IDatabaseSettings
+    {
+        public string ConnectionString => ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+        public int TickInterval => GetAppsettingInt("TickInterval", 5000);
+
+        private int GetAppsettingInt(string key, int defaultValue)
+        {
+            int dummy;
+            return int.TryParse(ConfigurationManager.AppSettings[key], out dummy) ? dummy : defaultValue;
+        }
+    }
+
+
 }

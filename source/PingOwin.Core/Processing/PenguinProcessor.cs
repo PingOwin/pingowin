ef4343af31a0f2022eae347e4dc921d88668d81b
John10 Korsnes <johnkors@gmail.com>
2016-05-05 01:51:29 +0200

    namings

--- source/PingOwin.Core/Processing/PenguinProcessor.cs
+++ source/PingOwin.Core/Processing/PenguinProcessor.cs
@@ -9,17 +9,17 @@ namespace PingOwin.Core.Processing
     {
         private readonly ILogger _log = Log.ForContext<PenguinProcessor>();
 
-        private readonly IPenguinRepository _repo;
-        private readonly IPenguinResultsRepository _penguinResultsRepository;
+        private readonly IPenguinRepository _urlRepo;
+        private readonly IPenguinResultsRepository _resultsRepo;
         private readonly ITransformResponses _transformer;
         private readonly HttpClientPinger _httpClientPinger;
         private readonly INotify _notifier;
 
-        public PenguinProcessor(IPingConfiguration config, IPenguinRepository penguinRepoi, IPenguinResultsRepository penguinResultsRepository, INotifierFactory notifierFactory, ITransformerFactory transformerfactory)
+        public PenguinProcessor(IPingConfiguration config, IPenguinRepository penguinRepoi, IPenguinResultsRepository resultsRepo, INotifierFactory notifierFactory, ITransformerFactory transformerfactory)
         {
             _httpClientPinger = new HttpClientPinger(config);
-            _repo = penguinRepoi;
-            _penguinResultsRepository = penguinResultsRepository;
+            _urlRepo = penguinRepoi;
+            _resultsRepo = resultsRepo;
 
             _transformer = transformerfactory.CreateTransformer();
             _notifier = notifierFactory.CreateNotifier();
@@ -27,21 +27,22 @@ namespace PingOwin.Core.Processing
 
         public async Task Tick()
         {
-            var penguins = await _repo.GetAll();
-            var urls = penguins.Where(c => c != null && !string.IsNullOrEmpty(c.Url)).Select(c => c.Url);
+            var urlsToPing = await _urlRepo.GetAll();
+            var urls = urlsToPing.Select(c => c.Url);
             var pingTasks = (from url in urls select _httpClientPinger.Ping(url)).ToList();
             var pingResults = await Task.WhenAll(pingTasks);
-
-            var orderedByLevel = pingResults.Where(c => c != null).OrderBy(c => c.Level).ToList();
-
-            foreach (var result in orderedByLevel)
+           
+            foreach (var result in pingResults)
             {
-                await _penguinResultsRepository.Insert(new PenguinResult
+                await _resultsRepo.Insert(new PenguinResult
                 {
                     Url = result.Url,
                     ResponseTime = (int)result.ResponseTime
                 });
             }
+
+            var orderedByLevel = pingResults.OrderBy(c => c.Level).ToList();
+
             var transformedMsg = _transformer.Transform(orderedByLevel);
             await _notifier.Notify(transformedMsg);
             _log.Information(transformedMsg);

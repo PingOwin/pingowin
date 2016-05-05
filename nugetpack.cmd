542ffd96edc41ac90f5231b4dff2175ec251a55f
John10 Korsnes <johnkors@gmail.com>
2016-05-05 11:29:29 +0200

    Cannot build C# 6 using msbuild

--- nugetpack.cmd
+++ nugetpack.cmd
@@ -1,2 +1,3 @@
 nuget restore ./source/PingOwin.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ./source/PingOwin.sln /p:Configuration=Release
 nuget pack ./source/PingOwin.Core.Frontend/PingOwin.Core.Frontend.nuspec -Build -OutputDirectory ./packed/ -Prop Configuration=Release

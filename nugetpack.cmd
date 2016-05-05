4a315962a84ddf131f002e28b13fa5feea36ec0e
John10 Korsnes <johnkors@gmail.com>
2016-05-05 11:31:08 +0200

    Switched to VS2015 msbuild to understand C# 6.0

--- nugetpack.cmd
+++ nugetpack.cmd
@@ -1,3 +1,3 @@
 nuget restore ./source/PingOwin.sln
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe ./source/PingOwin.sln /p:Configuration=Release
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ./source/PingOwin.sln /p:Configuration=Release
 nuget pack ./source/PingOwin.Core.Frontend/PingOwin.Core.Frontend.nuspec -Build -OutputDirectory ./packed/ -Prop Configuration=Release

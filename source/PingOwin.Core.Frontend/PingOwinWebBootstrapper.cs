bf5550a56289b0d3bb1c5857b0c8834ff2137a5c
John10 Korsnes <johnkors@gmail.com>
2016-05-10 20:49:29 +0200

    Removed views from the nuget package. Embedded resources instead

--- source/PingOwin.Core.Frontend/PingOwinWebBootstrapper.cs
+++ source/PingOwin.Core.Frontend/PingOwinWebBootstrapper.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Reflection;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
+using Nancy.ViewEngines;
 using PingOwin.Core.Interfaces;
 using PingOwin.Core.Store.SQLite;
 
@@ -19,7 +21,15 @@ namespace PingOwin.Core.Frontend
 
         protected override void ApplicationStartup(TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
         {
-            Conventions.ViewLocationConventions.Add((viewName, model, context) => String.Concat("bin/views/", viewName));
+            //Conventions.ViewLocationConventions.Add((viewName, model, context) => String.Concat("bin/views/", viewName));
+          
+        }
+
+        protected override NancyInternalConfiguration InternalConfiguration => NancyInternalConfiguration.WithOverrides(OnConfigurationBuilder);
+
+        void OnConfigurationBuilder(NancyInternalConfiguration x)
+        {
+            x.ViewLocationProvider = typeof(ResourceViewLocationProvider);
         }
 
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
@@ -27,6 +37,8 @@ namespace PingOwin.Core.Frontend
             container.Register<IDatabaseSettings>((c,p) => _databaseSettings);
             container.Register<IPenguinRepository, PenguinRepository>();
             container.Register<IPenguinResultsRepository, PenguinResultsRepository>();
+            var assembly = GetType().Assembly;
+            ResourceViewLocationProvider.RootNamespaces.Add(assembly, "PingOwin.Core.Frontend.Views");
             base.ConfigureApplicationContainer(container);
         }
 
